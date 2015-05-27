ff3e8fa524094dc8e9f10fcee652cc32b264b71c
Petar Ivanov <pepincho95@gmail.com>
2015-05-27 17:19:05 +0300

    add more payments functionality

--- checkout/views.py
+++ checkout/views.py
@@ -5,13 +5,17 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def checkout(request):
+    context = {}
     if request.method == "POST":
         # Set your secret key: remto your live secret key in production
         # See your keys here https://dashboard.stripe.com/account/apikeys
         stripe.api_key = "sk_test_dAIMPSAPAuWf8RAmypa0sDVf"
         token = request.POST['stripeToken']
+        print(request.POST['price'])
+        price = int(request.POST['price'])
+        price *= 100
 
-        charge = stripe.Charge.create(amount=1500, currency="usd", source=token, description="Example charge")
+        charge = stripe.Charge.create(amount=price, currency="usd", source=token, description="Example charge")
+        context['success'] = 'You paid successully!'
 
-    context = {}
     return render(request, "checkout.html", context)
