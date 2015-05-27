10b81093bd1f8edf9463aaa56771f864ae2625bf
Antonio FIlipov <tonkatafilipov@gmail.com>
2015-05-27 15:39:14 +0300

    add cart

--- front-end/init.js
+++ front-end/init.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
+
+    $("button#btn-add").on('click', function () {
+        var name = $(this).closest("li").find("h2").text();
+        // var price = $(".orderPrice").text();
+        if (name != "") {
+            OrderApp.addItemToCart(name, 12);
+        }
+    });
+
     $('#login').click(function(){
         $('#loginModule').toggle();
         $('#registrationModule').toggle();
-    })
+    });
 });
