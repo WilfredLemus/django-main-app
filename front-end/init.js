6a200a94bd189e6abb0684cf79f5fd5ae8ec6762
Antonio FIlipov <tonkatafilipov@gmail.com>
2015-05-27 16:13:48 +0300

    cart done

--- front-end/init.js
+++ front-end/init.js
@@ -6,10 +6,28 @@ $(document).ready(function(){
         if (name != "") {
             OrderApp.addItemToCart(name, 12);
         }
+        makeTable()
     });
 
     $('#login').click(function(){
         $('#loginModule').toggle();
         $('#registrationModule').toggle();
     });
+    makeTable()
+
+
+
+
 });
+
+function makeTable(){
+    var cart = JSON.parse(sessionStorage.getItem("cart"));
+    if (cart) {
+        var container = $("#orderTable");
+        var source = $("#cartTemplate").html();
+        var template = Handlebars.compile(source);
+        var context = {products:cart.products};
+        var html = template(context);
+        container.html(html);
+    };
+}
